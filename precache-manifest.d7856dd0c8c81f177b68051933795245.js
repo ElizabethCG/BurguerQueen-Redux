d27d405962e85ee05a39340823e90cb1a81f8ba5
ElizabethCG <elizabeth.caceres@gmail.com>
2019-06-12 19:17:10 -0400

    Updates

---
+++ precache-manifest.d7856dd0c8c81f177b68051933795245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a843f38d61c91b3c4eb3be0a690c644f",
+    "url": "/BurguerQueen-Redux/index.html"
+  },
+  {
+    "revision": "b7b922dd75b5313e01e6",
+    "url": "/BurguerQueen-Redux/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "aefd91736f69b7e32ecd",
+    "url": "/BurguerQueen-Redux/static/js/2.a84ae962.chunk.js"
+  },
+  {
+    "revision": "b7b922dd75b5313e01e6",
+    "url": "/BurguerQueen-Redux/static/js/main.c076d033.chunk.js"
+  },
+  {
+    "revision": "99717d5af2259791cfe6",
+    "url": "/BurguerQueen-Redux/static/js/runtime~main.66a95fd0.js"
+  }
+]);
